22269ff0edb31b6af4fd208efa16f7eb9d282da3
Luke W Johnston <lwjohnst@gmail.com>
2017-12-29 13:16:19 -0500

    trimmed down setup.R, removed load_data

--- R/setup.R
+++ R/setup.R
@@ -1,19 +1,5 @@
-# Setup description -------------------------------------------------------
+# Set options for all documents and scripts.
 #
-# These functions are used to setup your files so that all options are in one
-# location, and references via the function call.
-#
-# These functions can be accessed using either `library(projectname)` or
-# `devtools::load_all()` (Ctrl-Shift-L in RStudio).
-
-#' Set options for all documents and scripts.
-#'
-#' @export
-#'
-#' @examples
-#'
-#' set_options()
-#'
 set_options <- function() {
     # Set the options here for individual packages
 
