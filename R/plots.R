2c0e60560a3cd93bd08431b1d5c5493b09701400
Luke W Johnston <lwjohnst@gmail.com>
2018-01-08 17:30:32 -0500

    corr of laodings plot

--- R/plots.R
+++ R/plots.R
@@ -45,3 +45,56 @@ plot_scores <- function(.model) {
         scale_x_discrete(labels = c("1st", "2nd", "3rd")) +
         facet_wrap( ~ Component)
 }
+
+
+plot_corr_comps <- function(outcome, title = NULL) {
+    # xloadings <-
+    .model <- pls_model(project_data, outcome = outcome, ncomp = 2)
+    expl_var_50 <- sqrt(1 / 2)
+
+    fit <- cor(model.matrix(.model), pls::scores(.model)[, 1:2, drop = FALSE]) %>%
+        as_tibble(rownames = "xvariables") %>%
+        setNames(c('xvariables', 'C1', 'C2')) %>%
+        mutate(xvariables = PROMISE.misc::renaming_fa(xvariables) %>%
+                   stringr::str_replace("pct_", "")) %>%
+        mutate(Fraction = factor(stringr::str_extract(xvariables, "NE|TG|PL|CE"))) %>%
+        mutate(xvariables = ifelse(calc_radius(C1, C2) >= expl_var_50, xvariables, NA))
+
+    circle_outer <- seer:::.circle_data(1)
+    circle_inner <- seer:::.circle_data(sqrt(1 / 2))
+
+    fig <- ggplot(fit, aes_string(x = "C1", y = "C2")) +
+        geom_segment(aes(
+            x = -1,
+            y = 0,
+            xend = 1,
+            yend = 0
+        ), colour = 'grey90') +
+        geom_segment(aes(
+            x = 0,
+            y = -1,
+            xend = 0,
+            yend = 1
+        ), colour = 'grey90') +
+        geom_path(data = circle_outer, aes(x = x, y = y)) +
+        geom_path(data = circle_inner, aes(x = x, y = y), linetype = 'dotted') +
+        geom_point(data = fit, aes(colour = Fraction)) +
+        viridis::scale_color_viridis(discrete = TRUE) +
+        # scale_color_brewer(palette = "Set1") +
+        ggrepel::geom_text_repel(
+            data = fit,
+            aes(label = xvariables),
+            size = 3,
+            box.padding = 0.4,
+            segment.alpha = 0.3
+        ) +
+        labs(
+            x = paste0('C1 (', round(pls::explvar(.model)[1], 1), '%)'),
+            y = paste0('C2 (', round(pls::explvar(.model)[2], 1), '%)'),
+            title = title
+        ) +
+        theme_classic()
+    fig
+}
+
+
