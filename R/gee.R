2ca84f419aff30a18869010d2e88e884a319edfe
Luke W Johnston <lwjohnst@gmail.com>
2018-04-09 11:51:33 +0200

    removed all dplyr:: and mason::

--- R/gee.R
+++ R/gee.R
@@ -15,27 +15,27 @@ prep_gee_data <- function(outcome) {
 analyze_gee_pls <- function() {
 
     isi <- prep_gee_data(outcome = "lISI") %>%
-        mason::design('gee') %>%
-        mason::add_settings(family = stats::gaussian(),
+        design('gee') %>%
+        add_settings(family = stats::gaussian(),
                             corstr = 'ar1', cluster.id = 'SID') %>%
-        mason::add_variables('yvars', "lISI") %>%
-        mason::add_variables('xvars', c("Comp1", "Comp2")) %>%
-        mason::construct() %>%
-        mason::scrub() %>%
-        mason::polish_filter("Term", 'term') %>%
+        add_variables('yvars', "lISI") %>%
+        add_variables('xvars', c("Comp1", "Comp2")) %>%
+        construct() %>%
+        scrub() %>%
+        polish_filter("Term", 'term') %>%
         padjust_model_output() %>%
         mutate_at(vars(estimate, conf.low, conf.high), funs((exp(.) - 1) * 100))
 
     issi2 <- prep_gee_data(outcome = "lISSI2") %>%
-        mason::design('gee') %>%
-        mason::add_settings(family = stats::gaussian(),
+        design('gee') %>%
+        add_settings(family = stats::gaussian(),
                             corstr = 'ar1', cluster.id = 'SID') %>%
-        mason::add_variables('yvars', "lISSI2") %>%
-        mason::add_variables('xvars', c("Comp1", "Comp2")) %>%
+        add_variables('yvars', "lISSI2") %>%
+        add_variables('xvars', c("Comp1", "Comp2")) %>%
         add_variables("covariates", "YearsFromBaseline") %>%
-        mason::construct() %>%
-        mason::scrub() %>%
-        mason::polish_filter("Term", 'term') %>%
+        construct() %>%
+        scrub() %>%
+        polish_filter("Term", 'term') %>%
         padjust_model_output() %>%
         mutate_at(vars(estimate, conf.low, conf.high), funs((exp(.) - 1) * 100))
 
@@ -44,6 +44,6 @@ analyze_gee_pls <- function() {
 
 padjust_model_output <- function(gee_results) {
     gee_results %>%
-        mason::polish_adjust_pvalue(method = 'BH') %>%
-        dplyr::rename(unadj.p.value = p.value, p.value = adj.p.value)
+        polish_adjust_pvalue(method = 'BH') %>%
+        rename(unadj.p.value = p.value, p.value = adj.p.value)
 }
